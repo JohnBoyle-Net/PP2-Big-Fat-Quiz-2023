ab737db1a24b6f40e2d46c8ff6a2a111203fc770
Louise Benson <louise.benson@gmail.com>
2023-02-08 11:11:50 +0000

    Add 5 question and answers to questions array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,48 @@
-console.log("Hello World!")
+// questions and answers array. 
+let questions = [
+    {
+        question: 'Which country has the most Pyramids?',
+        answer: [
+            {text: 'Egypt', correct: false },
+            {text: 'Sudan', correct: true},
+            {text: 'Libya', correct: false},
+            {text: 'Israel', correct: false}
+        ]
+    },
+    {
+        question: 'Which country has the most highest waterfall?',
+        answer: [
+            {text: 'South Africa', correct: false },
+            {text: 'USA', correct: false},
+            {text: 'Venezuela', correct: true},
+            {text: 'Peru', correct: false}
+        ]
+    },
+    {
+        question: 'Which country has the most official languages?',
+        answer: [
+            {text: 'Bolivia', correct: true },
+            {text: 'Zimbabwe', correct: false},
+            {text: 'Singapore', correct: false},
+            {text: 'South Africa', correct: false}
+        ]
+    },
+    {
+        question: 'Which country has the most active volcanoes?',
+        answer: [
+            {text: 'Japan', correct: false},
+            {text: 'USA', correct: false},
+            {text: 'Indonesia', correct: true},
+            {text: 'Iceland', correct: false}
+        ]
+    },
+    {
+        question: "In which country is Europe's highest mountain",
+        answer: [
+            {text: 'Slovenia', correct: false },
+            {text: 'Russia', correct: true},
+            {text: 'Italy', correct: false},
+            {text: 'Switzerland', correct: false}
+        ]
+    }
+];
