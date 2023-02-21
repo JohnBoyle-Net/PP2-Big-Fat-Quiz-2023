e4ac314e06ad455c7fabacedf9e1ab8a115a43f5
Louise Benson <louise.benson@gmail.com>
2023-02-21 13:40:53 +0000

    work on function to display correct and incorrect answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -116,15 +116,33 @@ function resetState() {
     clearStatusClass(document.body)
     nextButton.classList.add('hide')
     while (answerButtonsElement.firstChild) {
-      answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+        buttonBox.removeChild(answerButtonsElement.firstChild)
     }
   }
 
 function selectAnswer(e) {
-
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(buttonBox.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
 }
 
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add('correct')
+    } else {
+        element.classList.add('incorrect')
+    }
+    
+}
 
+function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('incorrect')
+}
 
 
 
