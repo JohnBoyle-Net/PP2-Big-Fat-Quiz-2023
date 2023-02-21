7f3097e143e5f638bf5b1ff29500ac05f0cf18af
Louise Benson <louise.benson@gmail.com>
2023-02-21 12:55:03 +0000

    work on functions to display questions and answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-addEventListener('DOMContentLoaded', (event) => {
+
 
 
 // questions and answers arrays 
@@ -61,105 +61,104 @@ const playGameArea = document.getElementById('game-play')
 const questionBox = document.getElementById('question-box')
 const nextButton = document.getElementById('next-btn')
 const resultsArea = document.getElementById('game-results')
+const answerBoxes = document.getElementsByClassName('answers')
+const buttonBox = document.getElementById('button-box')
 
 // function to start the quiz by loading quiz area (question and answers) and the score display
 
 startButton.addEventListener('click', startGame)
 
+let shuffledQuestion = ""
+let currentQuestionIndex = ""
 
 
 function startGame() {
     console.log('started')
     quizRules.classList.add('hide')
     playGameArea.classList.remove('hide')
-    setFirstQuestion()
     displayNumberOfQuestions ()
-    displayCorrectAnswer()
+    questionsOrdered = questions
+    currentQuestionIndex = 0
+    setNextQuestion()
 }
 
 // function to calculate and display total number of questions. Total will increase if more questions are added.
 
-function displayNumberOfQuestions () {
+function displayNumberOfQuestions() {
     const NoOfQs = questions.length;
     document.getElementById('number-of-qs').innerText = NoOfQs;
 }
 
-// function to display the first question and the possible answers
-
-function setFirstQuestion() {
-    let i = 0; 
-    iLoop:
-    while (i < 5) {
-        let j = 0;
-        jLoop:
-        while (j < 5) {
-            if (j === 0) {
-        document.getElementById('four').innerHTML = questions[i].answer[j].text;
-        document.getElementById('three').innerHTML = questions[i].answer[j+1].text;
-        document.getElementById('one').innerHTML = questions[i].answer[j+2].text;
-        document.getElementById('two').innerHTML = questions[i].answer[j+3].text;  
-        }
-        
-        j++;
-    }
-    if (i === 0) {
-        
-        
-        questionBox.innerHTML= questions[i].question
-        break;
-    }
-   
-    i++;
-    
-    }
-} 
-
-// function to display correct answer. When user clicks on an answer it should turn either green or red. Correct answer will be green 
-// and all incorrect will be red
-
-// not working
-
-function displayCorrectAnswer(event) {
-    const answerBoxes = document.getElementsByClassName('answers')
-    if (questions.correct) {
-        answerBoxes.classList.add('correct');
-    } else {
-        answerBoxes.classList.add('incorrect');
-    };
-    incrementUserScore()
-    answerBoxes.addEventListener('click', displayCorrectAnswer())
-};
-
-
 function setNextQuestion() {
-    let i = 0; 
-    iLoop:
-    while (i < 5) {
-        let j = 0;
-        jLoop:
-        while (j < 5) {
-            if (j === 0) {
-                document.getElementById('four').innerHTML = questions[i+1].answer[j].text;
-                document.getElementById('three').innerHTML = questions[i+1].answer[j+1].text;
-                document.getElementById('one').innerHTML = questions[i+1].answer[j+2].text;
-                document.getElementById('two').innerHTML = questions[i+1].answer[j+3].text;  
-        }
-        
-        j++;
-    }
-    if (i === 0) {
-        
-        
-        questionBox.innerHTML= questions[i+1].question
-        break;
-    }
-   
-    i++;
+    showQuestion(questionsOrdered[currentQuestionIndex])
     
-    }
+        
+        
+}
+    
+
+function showQuestion(question) {
+    questionBox.innerText = question.question
+    question.answer.forEach(answer => {
+        const button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('answers')
+        if(answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+
+        button.addEventListener('click', selectAnswer)
+        buttonBox.appendChild(button)
+})
 }
 
-nextButton.addEventListener('click', setNextQuestion) 
+function resetState() {
+    clearStatusClass(document.body)
+    nextButton.classList.add('hide')
+    while (answerButtonsElement.firstChild) {
+      answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+    }
+  }
+
+function selectAnswer(e) {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // function to increase the score displayed when a correct answer is selected
 
@@ -192,9 +191,8 @@ function playAgain() {
 }
 
 
-}
 
 
 
 
-)
+
